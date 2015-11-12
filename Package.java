aa2ae16e91c776a9967865a527257b7d7d2df744
Xiang Wang <1846468966@qq.com>
2015-11-12 20:57:13 +0800

    删除重复文件

--- Package.java
+++
@@ -1,7 +0,0 @@
-import mypackage.Person;    // from mypackage directory import Person class
-public class Package {
-    public static void main(String[] args){
-        Person p = new Person();
-        System.out.println("Hello World!");    
-    }
-}
